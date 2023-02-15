2ed66ec104d395eba6053be2bd7c4a36efccfd09
Rdhkyuu <thecreepercraftindo31@gmail.com>
2023-02-15 09:42:02 +0800

    Menambah file index.pgp

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nyoba git remote lagi</title>
+</head>
+<body>
+    <h2>Database Siswa:</h2>
+    <table border='1'>
+        
+    </table>    
+
+
+</body>
+</html>
